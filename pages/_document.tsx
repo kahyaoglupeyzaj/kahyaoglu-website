bdbdff411b82e3ecb57b8880e1a751def3beb69e
kutay-vesti <kutay@vestiyer.co>
2023-06-19 13:59:14 +0300

    gtagjs

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
   return (
     <Html lang="tr">
       <Head >
-        <Script id="google-analytics" strategy="afterInteractive">
+        {/* <Script id="google-analytics" strategy="afterInteractive">
           {`
           window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
           ga('create', 'GTM-WFZSBBW', 'auto');
@@ -15,7 +15,22 @@ export default function Document() {
         <Script
           src="https://www.google-analytics.com/analytics.js"
           strategy="afterInteractive"
+        /> */}
+
+        {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=GTM-WFZSBBW"
+          strategy="afterInteractive"
         />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'GTM-WFZSBBW');
+        `}
+        </Script>
       </Head>
       <body className="bg-background-primary text-black">
         <Main />
